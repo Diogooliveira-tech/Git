562347537645eb4d4402830805b48e6021165e67
dmo_vortx <dmo@vortx.com.br>
2020-06-15 19:32:44 -0300

    primeira versão da automação em ruby

---
+++ spec/dropdown_spec.rb
@@ -0,0 +1,23 @@
+
+
+describe 'Caixa de seleção', :dropdown do 
+
+    it 'item especifico simples' do
+        visit '/dropdown'
+        select('Loki', from: 'dropdown')
+        sleep 3 #temporária
+    end
+
+    it 'item especifico com o find' do 
+        visit '/dropdown'
+        drop = find('.avenger-list')
+        drop.find('option', text: 'Scott Lang'). select_option
+        sleep 3 # temporário
+    end
+
+    it 'qualque item', :sample do
+        visit '/dropdown'
+        drop = find('.avenger-list')
+        drop.all('option').sample.select_option
+    end
+end
---
+++ spec/login_form_spec.rb
@@ -0,0 +1,42 @@
+
+
+describe 'Login Form', :smoke do 
+    
+    it 'login com sucesso' do
+        visit '/login'
+
+        fill_in 'userId', with: 'stark'
+        fill_in 'passId', with: 'jarvis!'
+
+        click_button 'Login'
+    
+        expect(find('#flash').visible?).to be true
+        expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+
+    end
+
+    it 'senha incorreta', :temp do
+        visit '/login'
+
+        fill_in 'userId', with: 'stark'
+        fill_in 'password', with: 'xpto123'
+        
+        click_button 'Login'
+
+        expect(find('#flash')).to have_content 'Senha é invalida!'
+  
+    end
+
+    it 'usuário não cadastrado' do
+        visit '/login'
+
+        fill_in 'userId', with: 'hulk'
+        fill_in 'password', with: 'xpto123'
+        
+        click_button 'Login'
+         
+        expect(find('#flash')).to have_content 'O usuário informado não cadastrado!'
+
+    end
+    
+end
